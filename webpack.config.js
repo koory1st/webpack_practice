f90a30e9e30897a0e3f48bda23665a7cc2e48186
koory1st <32436334@qq.com>
2018-05-09 19:19:56 +0800

    babel规则

--- webpack.config.js
+++ webpack.config.js
@@ -15,11 +15,22 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        }]
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 };
