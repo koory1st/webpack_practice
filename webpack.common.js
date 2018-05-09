980359fcd122e04bf19720de39390a7a5b270ac8
koory1st <32436334@qq.com>
2018-05-09 22:36:03 +0800

    添加模块热更新

--- webpack.common.js
+++ webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
         })
     ],
     output: {
-        filename: '[name].[chunkhash].js',
+        filename: '[name].[hash].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
