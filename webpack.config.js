2b435da39a62b232aa2b95365b870f45e205db9f
koory1st <32436334@qq.com>
2018-05-09 19:18:45 +0800

    防缓存

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
         })
     ],
     output: {
-        filename: '[name].bundle.js',
+        filename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
