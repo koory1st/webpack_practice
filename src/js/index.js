b0c61619c7aec53224ad343866c8a62482191538
koory1st <32436334@qq.com>
2018-05-29 17:58:30 +0800

    加入html的fontsize计算的js代码

--- src/js/index.js
+++ src/js/index.js
@@ -5,3 +5,8 @@ import '../img/b.jpg';
 console.log(1111);
 // var a;
 
+let htmlWidth = document.documentElement.clientWidth || document.body.clientWidth;
+
+let htmlDom = document.getElementsByTagName('html')[0];
+
+htmlDom.style.fontSize = htmlWidth / 10 + 'px';
